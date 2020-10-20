3341db524182903e0e726c2d7a7b76fdda98fecb
didit <aditiadidit4@gmail.com>
2020-10-20 20:06:37 +0700

    tambah halaman produk

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -5,10 +5,18 @@ class Admin extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('admin');
+        $data = array("content" => "");
+		$this->load->view('admin', $data);
     }
     
     public function login() {
         $this->load->view('admin/login');
+    }
+
+    public function produk() {
+
+       $content = $this->load->view('admin/produk', [], true);
+       $data = array("content" => $content);
+       $this->load->view('admin', $data);
     }
 }
---
+++ application/views/admin/produk.php
@@ -0,0 +1,59 @@
+ <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Produk</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Beranda</a></li>
+              <li class="breadcrumb-item active">Produk</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                  <div class="card-body">
+                <table id="produkTable" class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>Column 1</th>
+                        <th>Column 2</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Row 1 Data 1</td>
+                        <td>Row 1 Data 2</td>
+                    </tr>
+                    <tr>
+                        <td>Row 2 Data 1</td>
+                        <td>Row 2 Data 2</td>
+                    </tr>
+                </tbody>
+                </table>
+               </div>
+            </div>
+            </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+
+  <script>
+  $(document).ready( function () {
+        $('#produkTable').DataTable();
+  });
+  </script>
